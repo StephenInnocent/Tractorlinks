7ee7fef191d58675819c2173b5778f930b0c2398
Stephen Innocent <borokinnocent224@gmail.com>
2023-05-17 16:43:19 -0700

    added helmet security

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
-// path = ""
 const port = process.env.PORT || 9999;
 const {userRouter} = require("./src/routes/users.routes");
 const {orderRouter} = require("./src/routes/orders.routes");
@@ -13,12 +12,13 @@ require('dotenv').config();
 const MONGO_URL = process.env.MONGO_URL;
 
 
-
+app.use(helmet())
 app.use(express.json());
 app.use(cors());
 app.use("/orders", orderRouter);
 app.use("/users", userRouter);
-app.use("/admin", adminRouter)
+app.use("/admin", adminRouter);
+app.disable('x-powered-by');
 
 
 app.get("/", (req,res) => {
